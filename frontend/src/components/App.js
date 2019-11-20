3b03748396efa2e0ffde1d0d2ed5708a829638a9
pauli varelius <pauli.varelius@gmail.com>
2019-11-20 21:33:57 +0200

    added socket.io to frontend and backend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const socket = io('http://localhost:8080');
   return (
     <div className="App">
       <header className="App-header">
