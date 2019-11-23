45e1076f9caa74462fb4926b5524e81c0a09843a
pauli varelius <pauli.varelius@gmail.com>
2019-11-24 00:55:02 +0200

    added scss and did more UI work

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import io from 'socket.io-client';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Fab from "@material-ui/core/Fab";
+import SendIcon from '@material-ui/icons/Send';
+import TextField from '@material-ui/core/TextField';
+import InputBase from '@material-ui/core/InputBase';
 
 function App() {
   const socket = io('http://localhost:8080');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+        <div className="messagesWrapper">
+          <div className="messages">
+            <div className="message">Nightyyyy &lt;3 &lt;3 &lt;3 </div>
+          </div>
+        </div>
+        <div className="messagingWrapper">
+        <InputBase className="inputbox" inputProps={{ 'aria-label': 'naked' }}/>
+          <div className="button">
+            <Fab color="default" aria-label="send" size="small">
+              <SendIcon  style={{fontSize: "100%"}}/>
+            </Fab>
+          </div>
+        </div>
+      </div>
   );
 }
 
