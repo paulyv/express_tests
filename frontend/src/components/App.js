4eaf40ac0aedde774a6b5a3ba0b5e37ddf166af1
pauli varelius <pauli.varelius@gmail.com>
2019-11-26 00:03:42 +0200

    changed the css positions - not working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -10,21 +10,41 @@ import InputBase from '@material-ui/core/InputBase';
 function App() {
   const socket = io('http://localhost:8080');
   return (
-      <div className="wrapper">
+    <div className="wrapper">
         <div className="messagesWrapper">
-          <div className="messages">
-            <div className="message">Nightyyyy &lt;3 &lt;3 &lt;3 </div>
-          </div>
+            <div className="messages">
+              <div className="message">message 1 </div>
+              <div className="message">message 2 </div>
+              <div className="message">message 3 </div>
+              <div className="message">message 4 </div>
+              <div className="message">message 5 </div>
+              <div className="message">message 6 </div>
+
+              <div className="message">message 1 </div>
+              <div className="message">message 2 </div>
+              <div className="message">message 3 </div>
+              <div className="message">message 4 </div>
+              <div className="message">message 5 </div>
+              <div className="message">message 6 </div>
+
+              <div className="message">message 1 </div>
+              <div className="message">message 2 </div>
+              <div className="message">message 3 </div>
+              <div className="message">message 4 </div>
+              <div className="message">message 5 </div>
+              <div className="message">message 6 </div>
+
+            </div>
         </div>
         <div className="messagingWrapper">
-        <InputBase className="inputbox" inputProps={{ 'aria-label': 'naked' }}/>
-          <div className="button">
-            <Fab color="default" aria-label="send" size="small">
-              <SendIcon  style={{fontSize: "100%"}}/>
-            </Fab>
-          </div>
+            <InputBase className="inputbox" inputProps={{ 'aria-label': 'naked' }}/>
+            <div className="button">
+                 <Fab color="default" aria-label="send" size="small">
+                      <SendIcon  style={{fontSize: "100%"}}/>
+                 </Fab>
+            </div>
         </div>
-      </div>
+    </div>
   );
 }
 
